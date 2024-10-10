ac03f1044f09eada7ce870e21229d546d78140cc
Baptiste Salaud <baptiste.salaud@epitech.eu>
2024-10-10 14:24:25 +0200

    feat: select user

--- front/gui/src/components/UserComponent.vue
+++ front/gui/src/components/UserComponent.vue
@@ -1,36 +1,66 @@
 <template>
   <div class="user">
     <MDBContainer>
-      <div class="d-flex justify-content-start align-items-center flex-column">
-        <h2>Create a user</h2>
-        <div class="text-center mb-3">
-          <form @submit.prevent="createUser">
-             <MDBInput 
-              label="Username" 
-              v-model="username" 
-              required
-              class="mb-3"
-            />
-            <MDBInput
-              label="Email" 
-              v-model="email" 
-              required
-              class="mb-3"
-            />
-            <MDBBtn type="submit" class="
-              btn btn-primary
-              btn-block
-              mb-4">Create user</MDBBtn>
-          </form>
-          <div v-if="message" class="mt-3">{{ message }}</div>
-        </div>
-      </div>
+      <MDBRow>
+        <MDBCol>
+          <h2>Select a user</h2>
+          <select
+            v-model="selectedUserId"
+            @change="selectUser"
+            class="
+              form-select
+              form-select-lg
+              mb-3
+              mt-3
+              col-6
+            "
+          >
+            <option disabled value="">Select a user</option>
+            <option v-for="user in users" :key="user.id" :value="user.id">
+              {{ user.username }}
+            </option>
+          </select>
+        </MDBCol>
+        <MDBCol>
+          <h2>Create a user</h2>
+          <div class="text-center mb-3">
+            <form @submit.prevent="createUser">
+              <MDBInput 
+                label="Username" 
+                v-model="createUsername"
+                required
+                class="mb-3"
+              />
+              <MDBInput
+                label="Email" 
+                v-model="createEmail" 
+                required
+                class="mb-3"
+              />
+              <MDBBtn type="submit" class="
+                btn btn-primary
+                btn-block
+                mb-4">Create user</MDBBtn>
+            </form>
+            <div v-if="message" class="mt-3">{{ message }}</div>
+          </div>
+        </MDBCol>
+      </MDBRow>
+      <MDBRow>
+        <MDBCol>
+          <h2>Selected user</h2>
+          <div v-if="username || email">
+            <p><strong>Username:</strong> {{ username }}</p>
+            <p><strong>Email:</strong> {{ email }}</p>
+          </div>
+        </MDBCol>
+      </MDBRow>
     </MDBContainer>
   </div>
 </template>
 
 <script>
-import { MDBBtn, MDBContainer, MDBInput } from "mdb-vue-ui-kit";
+import { MDBBtn, MDBContainer, MDBInput, MDBRow, MDBCol } from "mdb-vue-ui-kit";
 
 export default {
   name: "UserComponent",
@@ -38,16 +68,42 @@ export default {
     MDBContainer,
     MDBInput,
     MDBBtn,
+    MDBCol,
+    MDBRow
   },
   data() {
     return {
       username: "",
       email: "",
       users: [],
-      message: ""
+      selectedUserId: "",  // This will hold the selected user ID
+      message: "",
+      createUsername: "",
+      createEmail: ""
     };
   },
+  mounted() {
+    this.getUsers();
+  },
   methods: {
+    async getUsers() {
+      try {
+        const response = await fetch("http://localhost:4000/api/users");
+        this.users = (await response.json()).data;
+      } catch (error) {
+        this.message = "Error getting users: " + error.message;
+      }
+    },
+    selectUser() {
+      const selectedUser = this.users.find(user => user.id === this.selectedUserId);
+      if (selectedUser) {
+        this.username = selectedUser.username;
+        this.email = selectedUser.email;
+      } else {
+        this.username = "";
+        this.email = "";
+      }
+    },
     async createUser() {
       try {
         const response = await fetch("http://localhost:4000/api/users", {
@@ -57,7 +113,7 @@ export default {
           },
           body: JSON.stringify({
             user: {
-              username: this.username,
+              username: this.createUsername,
               email: this.email
             }
           })
