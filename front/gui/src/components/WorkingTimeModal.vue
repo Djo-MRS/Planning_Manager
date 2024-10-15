782e0b619fca2c4592978ca1f1eeed9d9b24a6f6
Baptiste Salaud <baptiste.salaud@epitech.eu>
2024-10-15 14:04:17 +0200

    feat: mirror and rebuild auto api

--- front/gui/src/components/WorkingTimeModal.vue
+++ front/gui/src/components/WorkingTimeModal.vue
@@ -2,133 +2,153 @@
   <main>
     <RouterView />
     <MDBModal
-          id="exampleModal"
-          tabindex="-1"
-          aria-labelledby="exampleModalLabel"
-          v-model="localShowModal"
-      >
-          <MDBModalHeader>
-              <MDBModalTitle id="exampleModalLabel"> {{currentDate}} {{isMorning ? "Morning": "Afternoon" }} </MDBModalTitle>
-          </MDBModalHeader>
-          <MDBModalBody>
-              <div class="data_row d-flex">
-                  <label for="start">Start </label>
-                  <input id="start" type="time" v-model="start" />
+      id="exampleModal"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      v-model="localShowModal"
+    >
+      <MDBModalHeader>
+        <MDBModalTitle id="exampleModalLabel">
+          {{ currentDate }} {{ isMorning ? "Morning" : "Afternoon" }}
+        </MDBModalTitle>
+      </MDBModalHeader>
+      <MDBModalBody>
+        <div class="data_row d-flex">
+          <label for="start">Start </label>
+          <input id="start" type="datetime-local" v-model="start" />
 
-                  <label for="end">End </label>
-                  <input id="end" type="time" v-model="end" />
-              
-              </div>
-          </MDBModalBody>
-          <MDBModalFooter>    
-              <MDBBtn color="primary" @click="savingWorkingTime">Save changes</MDBBtn>
-              <MDBBtn color="secondary" @click="clearAllData">Clear data</MDBBtn>
-          </MDBModalFooter>
-      </MDBModal> 
+          <label for="end">End </label>
+          <input id="end" type="datetime-local" v-model="end" />
+        </div>
+      </MDBModalBody>
+      <MDBModalFooter>
+        <MDBBtn color="primary" @click="savingWorkingTime">Save changes</MDBBtn>
+        <MDBBtn color="secondary" @click="clearAllData">Clear data</MDBBtn>
+      </MDBModalFooter>
+    </MDBModal>
   </main>
 </template>
 
 <script setup>
-  import { MDBBtn, MDBModal, MDBModalBody, MDBModalFooter, MDBModalHeader, MDBModalTitle } from 'mdb-vue-ui-kit';
-  import { RouterView } from 'vue-router';
-  import { defineProps, defineEmits, onMounted, computed, ref,  } from 'vue';
+  import {
+    MDBBtn,
+    MDBModal,
+    MDBModalBody,
+    MDBModalFooter,
+    MDBModalHeader,
+    MDBModalTitle,
+  } from "mdb-vue-ui-kit";
+  import { RouterView } from "vue-router";
+  import { defineProps, defineEmits, onMounted, computed, ref } from "vue";
 
   const props = defineProps({
-      showModal: {
-          type: Boolean,
-          required: true
-      },
-      currentDate :{
-          type: Date,
-          required: true,
-      },
-      period: {
-          type: Object,
-      },
-      current_user: {
-          type: Object,
-      },
-
+    showModal: {
+      type: Boolean,
+      required: true,
+    },
+    currentDate: {
+      type: Date,
+      required: true,
+    },
+    period: {
+      type: Object,
+    },
+    current_user: {
+      type: Object,
+    },
   });
-  const start = ref(props.period.start);
-  const end = ref(props.period.end);
+
+  const start = ref("");
+  const end = ref("");
   const isEmpty = ref(true);
   let isMorning = true;
-  const formatTime=()=>{
-      if (props.period && props.period.start) {
-          const startDate = new Date(props.period.start);
-          const hours = startDate.getHours();
-          
-          const isBeforeNoon = hours < 12;
 
-          if (isBeforeNoon) {
-              console.log("C'est le matin.");
-              isMorning = true
-          } else {
-              console.log("C'est l'après-midi ou le soir.");
-              isMorning = false
-          }
-          start.value = startDate.toTimeString().slice(0, 5);
-          const endDate = new Date(props.period.end);
-          end.value = endDate.toTimeString().slice(0, 5);
-      } else {
-          console.log("La date de début n'est pas définie.");
-      }
-  }
+  const formatDateTime = (date) => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    console.log(`${year}-${month}-${day}T${hours}:${minutes}`);
 
-  onMounted(()=>{
-      formatTime()
-      isEmpty.value = !(start.value && end.value);
+    return `${year}-${month}-${day}T${hours}:${minutes}`;
+  };
+
+  const formatTime = () => {
+    if (props.period && props.period.start) {
+      const startDate = new Date(props.period.start);
+      const endDate = new Date(props.period.end);
+
+      const hours = startDate.getHours();
+      isMorning = hours < 12;
+
+      start.value = formatDateTime(startDate);
+      end.value = formatDateTime(endDate);
+    } else {
+      console.log("Start date is not defined.");
+    }
+  };
+
+  onMounted(() => {
+    console.log(props.period);
+    formatTime();
+    isEmpty.value = !(start.value && end.value);
   });
 
-  const emit = defineEmits(['closeModal']);
+  const emit = defineEmits(["closeModal"]);
   const localShowModal = computed({
-      get: ()=> props.showModal, 
-      set: (value) => {
-          if(!value){
-              emit('closeModal')
-          }
+    get: () => props.showModal,
+    set: (value) => {
+      if (!value) {
+        emit("closeModal");
       }
+    },
   });
-  const clearAllData=async()=>{
-      try {
-          await fetch(`http://localhost:4000/api/workingtime/${props.current_user.id}/${props.period.id}`, {
-              method: "DELETE",
-              headers: {
-                  "Content-Type": "application/json"
-              },
-          });
-      } catch (error) {
-          console.log('Error', error);
-      }
-  }
 
-  const savingWorkingTime=async()=>{
-      try {
-          await fetch(`http://localhost:4000/api/workingtime/${props.period.id}`, {
-              method: "PUT",
-              headers: {
-                  "Content-Type": "application/json"
-              },
-              body: JSON.stringify({
-                  workingtime: {
-                      start: start,
-                      end: end
-                  }
-              })
-          });
-      } catch (error) {
-          console.log('Error', error);
-      }
-  }
+  const clearAllData = async () => {
+    try {
+      await fetch(
+        `http://localhost:4000/api/workingtime/${props.current_user.id}/${props.period.id}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    } catch (error) {
+      console.log("Error", error);
+    }
+  };
+
+  const savingWorkingTime = async () => {
+    console.log(props.period);
+    try {
+      await fetch(`http://localhost:4000/api/workingtime/${props.period.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          workingtime: {
+            start: new Date(start.value),
+            end: new Date(end.value),
+          },
+        }),
+      });
+    } catch (error) {
+      console.log("Error", error);
+    }
+  };
 </script>
 
-<style>
-  #app {
-    font-family: Roboto, Helvetica, Arial, sans-serif;
-  }
 
-  .data_row{
-    justify-content: center;
-  }
+<style>
+#app {
+  font-family: Roboto, Helvetica, Arial, sans-serif;
+}
+
+.data_row {
+  justify-content: center;
+}
 </style>
