4ad8f138e4d97656adb2d1dddea89cfed7096d8e
Baptiste Salaud <baptiste.salaud@epitech.eu>
2024-10-24 17:28:21 +0200

    feat: add delete sign up admin

--- front/gui/src/components/SignupComponent.vue
+++ front/gui/src/components/SignupComponent.vue
@@ -1,56 +1,94 @@
 <template>
   <div class="signup-container">
     <img src="@/assets/logohometimemanager.jpg" alt="Logo" class="logo" />
-    <form @submit.prevent="createUser" class="signup-form">
-     <input
-        type="text"
-        v-model="lastname"
-        placeholder="Lastname"
-        required
-        class="input-field"
-      />
-      <input
-        type="text"
-        v-model= "firstname"
-        placeholder="Firstname"
-        required
-        class="input-field"
-      />
+    <MDBRow>
+      <MDBCol>
+        <h2>Créer un utilisateur</h2>
+          <form @submit.prevent="createUser" class="signup-form">
+          <input
+              type="text"
+              v-model="lastname"
+              placeholder="Lastname"
+              required
+              class="input-field"
+            />
+            <input
+              type="text"
+              v-model= "firstname"
+              placeholder="Firstname"
+              required
+              class="input-field"
+            />
 
-      <input
-        type="email" 
-        v-model="email"
-        placeholder="Email"
-        required
-        class="input-field"
-      />
-      <input
-        type="int"
-        v-model="role_id"
-        placeholder="Role"
-        required
-        class="input-field"
-      />
-      <input
-        type="password"
-        v-model="password"
-        placeholder="Password"
-        class="input-field"
-      />
-      <input
-        type="password"
-        v-model="confirmPassword"
-        placeholder="Confirm Password"
-        class="input-field"
-      />
-      <button type="submit" class="sign-up-button">Inscription</button>
-    </form>
+            <input
+              type="email" 
+              v-model="email"
+              placeholder="Email"
+              required
+              class="input-field"
+            />
+            <input
+              type="int"
+              v-model="role_id"
+              placeholder="Role"
+              required
+              class="input-field"
+            />
+            <input
+              type="password"
+              v-model="password"
+              placeholder="Password"
+              class="input-field"
+            />
+            <input
+              type="password"
+              v-model="confirmPassword"
+              placeholder="Confirm Password"
+              class="input-field"
+            />
+            <button type="submit" class="sign-up-button">Inscription</button>
+          </form>
+
+          <div v-if="message" class="mt-3">{{ message }}</div>
+
+      </MDBCol>
+
+      <MDBCol>
+        <h2>Supprimer un utilisateur</h2>
+          <form @submit.prevent="deleteUser" class="signup-form">
+        <select
+          v-model="selectedUserId"
+          @change="selectUser"
+          class="
+            form-select
+            form-select-lg
+            mb-3
+            mt-3
+            col-6
+          ">
+          <option disabled value="">Select a user</option>
+          <option v-for="user in users" :key="user.id" :value="user.id">
+            {{ user.username }}
+          </option>
+        </select>
+        <button type="submit" class="sign-up-button">Supprimer</button>
+      </form>
+
+      <div v-if="messageDeleted" class="mt-3">{{ messageDeleted }}  </div>
+      </MDBCol>
+    </MDBRow>
   </div>
 </template>
 
 <script>
 
+import { MDBRow, MDBCol } from 'mdb-vue-ui-kit';
+
 export default {
+  components: {
+    MDBRow,
+    MDBCol,
+  },
   data() {
     return {
       lastname: '',
@@ -59,13 +97,26 @@ export default {
       role_id: 1,
       password: '',
       confirmPassword: '',
+      message: '',
+      users: [],
+      selectedUserId: '',
+      messageDeleted: '',
     };
   },
+  mounted() {
+    this.getUsers();
+  },
   methods: {
-  
+
+    selectUser() {
+      const user = this.users.find(user => user.id === this.selectedUserId);
+      this.username = user.username;
+      this.email = user.email;
+    },
+
   async createUser() {
       try {
-        const response = await fetch("http://localhost:4000/api/users/sign_up", {
+        const response = await fetch("/api/users/sign_up", {
           method: "POST",
           headers: {
             "Content-Type": "application/json"
@@ -82,17 +133,44 @@ export default {
         });
 
         if (response.ok) {
-     //     this.message = "User created successfully!";
+         this.message = "User created successfully!";
           this.username = "";
           this.email = "";
-        // this.messageDeleted = "";
-        //  this.messageUpdated = ""; -->
-      //    await this.getUsers(); // Refresh user list
+          this.messageDeleted = "";
+          this.messageUpdated = "";
         } else {
-      //    this.message = "Error creating user: " + response.statusText;
+         this.message = "Error creating user: " + response.statusText;
         }
       } catch (error) {
-     //   this.message = "Error creating user: " + error.message;
+       this.message = "Error creating user: " + error.message;
+      }
+    },
+
+    async getUsers() {
+      try {
+        const response = await fetch("/api/users");
+        this.users = (await response.json()).data;
+      } catch (error) {
+        this.message = "Error getting users: " + error.message;
+      }
+    },
+
+    async deleteUser() {
+      try {
+        const response = await fetch("/api/users/" + this.selectedUserId, {
+          method: "DELETE"
+        });
+
+        if (response.ok) {
+          this.messageDeleted = "User deleted successfully!";
+          this.message = "";
+          this.messageUpdated = "";
+          await this.getUsers();
+        } else {
+          this.messageDeleted = "Error deleting user: " + response.statusText;
+        }
+      } catch (error) {
+        this.messageDeleted = "Error deleting user: " + error.message;
       }
     },
 }
